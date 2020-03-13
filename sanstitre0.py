5f66228e77098b8926a79c87bb2522a848ab4d65
victorius35 <victor.bertret@gmail.com>
2020-03-13 15:10:49 +0100

    Update sanstitre0.py

--- sanstitre0.py
+++ sanstitre0.py
@@ -5,6 +5,12 @@ Created on Fri Mar 13 14:43:03 2020
 @author: victo
 """
 
-print("Hello World")
-a=10
-print(a)
+liste=dict()
+liste[1]={ 2 : 2, 3 : 7}
+liste[2]={ 1 : 3, 3 : 7}
+print(liste)
+for x in liste.keys():
+    print(x , ": ", list(liste[x].keys()))
+
+#%%
+liste1=[[i for i in liste[j].values()] for j in liste.keys()]
