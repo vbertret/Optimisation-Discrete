d65c7fd39c43242850097b1454eb00707a863ae8
victorius35 <victor.bertret@gmail.com>
2020-03-13 16:38:33 +0100

    Update sanstitre0.py

--- sanstitre0.py
+++ sanstitre0.py
@@ -7,6 +7,14 @@ print(liste)
 for x in liste.keys():
     print(x , ": ", list(liste[x].keys()))
 
+min=10000
+index=0
+for i in liste.keys():
+    for j in liste[i].keys():
+        if(liste[i][j]<min):
+            min=liste[i][j]
+            index=(i,j)
+
 #%%
 liste1=[[i for i in liste[j].values()] for j in liste.keys()]
 
