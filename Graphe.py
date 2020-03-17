a0e413eba5613fc06b5628b1c554ed2639b92845
victorius35 <victor.bertret@gmail.com>
2020-03-17 20:44:58 +0100

    Update Graphe.py

--- Graphe.py
+++ Graphe.py
@@ -27,7 +27,8 @@ class Graphe():
             self.ar[depart][arrivee]=[]
         self.ar[depart][arrivee].append(val)
         self.nbA+=1
-        
+      
+    #Creation d'une copie d'un graphe avec une adresse mémoire différente   
     def copie(self):
         Gcopie=Graphe()
         for sommet in self.so:
@@ -38,6 +39,20 @@ class Graphe():
                 Gcopie.ar[i][j]=list(self.ar[i][j])
                 Gcopie.nbA+=len(self.ar[i][j])
         return(Gcopie)
+     
+    #Supression d'une arrête
+    def enleverArrete(self,depart,arrivee,val):
+        if(not(depart in self.ar.keys() and arrivee in self.ar[depart].keys() and val in self.ar[depart][arrivee])):
+            print("Cette arrête n'existe pas !")
+            return(False)
+        self.ar[depart][arrivee].remove(val)
+        self.nbA-=1
+        if(len(self.ar[depart][arrivee])==0):
+            del(self.ar[depart][arrivee])
+        if(len(self.ar[depart])==0):
+            del(self.ar[depart])
+        return(True)
+        
 
         
 if __name__ == "__main__":
@@ -48,5 +63,6 @@ if __name__ == "__main__":
     G1.ajouterArrete("A","C",3)
     G1.ajouterArrete("A","B",10)
     G1.ajouterArrete("B","A",2)
+    G1.enleverArrete("B","A",2)
     
         
